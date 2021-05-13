0766702c5db7a5569d7cff8cae28bda50b21ec6f
Anjana Perera <Anjana.Perera@monash.edu>
2021-05-14 01:05:18 +1000

    Add debug logs in stopping criteria and breed next generation

--- ft/ga/genetic_algorithm.py
+++ ft/ga/genetic_algorithm.py
@@ -70,10 +70,12 @@ class GeneticAlgorithm:
         return run_time_sec
 
     def stopping_criteria(self):
+        self.logger.debug('Current generation - %d, max generations - %d' % (self.current_generation, self.max_generations))
         if self.current_generation > self.max_generations:
             return True
 
         run_time_sec = self.time_taken_since_start()
+        self.logger.debug('Current run time - %d seconds, maximum run time - %d seconds' % (run_time_sec, self.max_time))
         if run_time_sec > self.max_time:
             return True
         else:
@@ -114,19 +116,30 @@ class GeneticAlgorithm:
         return solution
 
     def breed_next_generation(self):
+        self.logger.debug('Start breeding next generation...')
         offspring_population = np.empty(self.population_size + self.num_test_insertion, dtype=TestCase)
 
         for offspring_round_index in range(0, int(self.population_size / 2)):
+            self.logger.debug('Offspring round index - %d | selecting parents...' % offspring_round_index)
             parents = self.selection_function.select_parents(self.population, 2)
+            self.logger.debug('Offspring round index - %d | parent 1 (ignore fitness) - [%s]' % (offspring_round_index, parents[0].to_string()))
+            self.logger.debug('Offspring round index - %d | parent 2 (ignore fitness) - [%s]' % (offspring_round_index, parents[1].to_string()))
 
+            self.logger.debug('Offspring round index - %d | crossing over parents...' % offspring_round_index)
             children_interim = self.crossover_function.crossover(parents[0], parents[1])
+            self.logger.debug('Offspring round index - %d | child interim 1 (ignore fitness) - [%s]' % (offspring_round_index, children_interim[0].to_string()))
+            self.logger.debug('Offspring round index - %d | child interim 2 (ignore fitness) - [%s]' % (offspring_round_index, children_interim[1].to_string()))
             self.input_validate_function(children_interim[0].get_test_input())
             self.input_validate_function(children_interim[1].get_test_input())
 
+            self.logger.debug('Offspring round index - %d | Mutating interim child 1...' % offspring_round_index)
             child_1 = self.mutation_function.mutate(children_interim[0])
+            self.logger.debug('Offspring round index - %d | child 1 (ignore fitness) - [%s]' % (offspring_round_index, child_1.to_string()))
             self.input_validate_function(child_1.get_test_input())
 
+            self.logger.debug('Offspring round index - %d | Mutating interim child 2 in middle of parents...' % offspring_round_index)
             child_2 = self.mutation_function.mutate_middle(children_interim[1], parents[0], parents[1])
+            self.logger.debug('Offspring round index - %d | child 2 (ignore fitness) - [%s]' % (offspring_round_index, child_2.to_string()))
             self.input_validate_function(child_2.get_test_input())
 
             self.fitnesses[self.population_size + offspring_round_index * 2] = self.fitness_function.evaluate_fitness(child_1)
@@ -134,6 +147,8 @@ class GeneticAlgorithm:
 
             offspring_population[offspring_round_index * 2] = child_1
             offspring_population[offspring_round_index * 2 + 1] = child_2
+            self.logger.debug('Offspring population index - %d | child 1 - [%s]' % (offspring_round_index * 2, child_1.to_string()))
+            self.logger.debug('Offspring population index - %d | child 2 - [%s]' % (offspring_round_index * 2 + 1, child_2.to_string()))
 
             if self.archive_test_inputs:
                 self.archive_test_input(child_1)
@@ -145,10 +160,12 @@ class GeneticAlgorithm:
             self.fitnesses[self.population_size * 2 + test_insertion_index] = self.fitness_function.evaluate_fitness(testcase)
 
             offspring_population[self.population_size + test_insertion_index] = testcase
+            self.logger.debug('Test insertion index - %d | [%s]' % (test_insertion_index, testcase.to_string()))
 
             if self.archive_test_inputs:
                 self.archive_test_input(testcase)
 
+        self.logger.debug('Finished breeding next generation.')
         return offspring_population
 
     def elitism(self, union_population):
@@ -193,6 +210,7 @@ class GeneticAlgorithm:
         self.logger.debug('current generation - %d, current fitness - %.4f' % (self.current_generation,
                                                                                self.fitnesses[0]))
 
+        self.logger.debug('Start evolving test cases...')
         while not self.stopping_criteria():
             self.evolve()
 
